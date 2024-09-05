a2397ab94c280b4e899051134da9860ecd07b6c0
ZeroNine <coolEmail@awesomeness.com>
2024-09-05 10:22:13 +0500

    Touch up for the login.

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -2,33 +2,46 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
+import { CiLock } from "react-icons/ci";
+import { IoPersonCircleSharp } from "react-icons/io5";
 import Image from "next/image";
 import React from "react";
 
 function page() {
   return (
     <>
-      <div className="flex flex-col h-max-[29.313rem] w-[22.813rem] items-center border-2">
+      <div className="flex flex-col h-max-[29.313rem] w-[22.813rem] items-center">
         <h2 className="mb-3 text-3xl font-bold text-center">Login</h2>
         <p>How do I get started lorem ipsum dolar at?</p>
         <div className="flex flex-col items-center w-full mt-7">
-          <Input placeholder="Username" className="bg-[#F0EDFF] h-14" />
-          <Input
-            placeholder="Password"
-            className="mt-[1.125rem] bg-[#F0EDFF] h-14"
-          />
+          <div className="flex bg-[#F0EDFF] h-14 w-full">
+            <span class="flex items-center leading-normal bg-grey-lighter rounded rounded-r-none border border-r-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-2xl">
+              <IoPersonCircleSharp />
+            </span>
+            <Input
+              placeholder="Username"
+              className=" bg-[#F0EDFF] h-14 rounded-l-none border border-l-0 "
+            />
+          </div>
+          <div className="flex mt-[1.125rem] bg-[#F0EDFF] h-14 w-full">
+            <span class="flex items-center leading-normal bg-grey-lighter rounded rounded-r-none border border-r-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-2xl">
+              <CiLock/>
+            </span>
+            <Input
+              placeholder="Password"
+              className=" bg-[#F0EDFF] h-14 rounded-l-none border border-l-0 "
+            />
+          </div>
           <Button className="bg-gradient-to-r from-[#9181F4] to-[#5038ED] w-[7.75rem] h-[3.25rem] mt-6">
             Login
           </Button>
           <p className="mt-6">Login with others</p>
           <div className="flex flex-col w-full mt-6 space-y-4">
-            <Button className="w-full h-14 bg-[#F0EDFF] text-black hover:text-white">
-              {" "}
+            <Button className="w-full h-14 bg-[#F0EDFF] text-black hover:text-white  text-lg">
               <FaGoogle /> Login with Google
             </Button>
-            <Button className="w-full h-14 bg-[#F0EDFF] text-black hover:text-white">
-              {" "}
-              <FaFacebookSquare /> Login with FAcebook
+            <Button className="w-full h-14 bg-[#F0EDFF] text-black hover:text-white text-lg">
+              <FaFacebookSquare /> Login with Facebook
             </Button>
           </div>
         </div>
